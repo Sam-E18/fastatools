0e667216cd667bfc52d4c5a8b8c75194f0b87ac7
Sam-E18 <samuel.escu18@gmail.com>
2024-10-19 09:25:56 +0200

    second commit :)

--- fastatitles.sh
+++ fastatitles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # fastatitles script to print all titles from fasta/fa files
-# in the current directory and subfolders
-
-# Use find to search for .fasta and .fa filesfind 
 
 grep -h  "^>" $(find . -type f -name "*.fa" -o -name "*.fasta")
+
+echo :)
