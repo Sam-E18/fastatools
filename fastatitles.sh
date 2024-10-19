c5686f6288c332b151cfe85c1efa69026a4aec5b
Sam-E18 <samuel.escu18@gmail.com>
2024-10-19 08:47:06 +0200

    I add the fastatitles

--- fastatitles.sh
+++ fastatitles.sh
@@ -3,5 +3,6 @@
 # fastatitles script to print all titles from fasta/fa files
 # in the current directory and subfolders
 
-# Use find to search for .fasta and .fa files
-find . -type f \( -name "*.fasta" -o -name "*.fa" \) -exec awk '/^>/{print substr($0, 2)}' {} \;
+# Use find to search for .fasta and .fa filesfind 
+
+grep -h  "^>" $(find . -type f -name "*.fa" -o -name "*.fasta")
